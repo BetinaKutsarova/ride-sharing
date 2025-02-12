74e000dd87d0d3a9bf4bd4594ebaff681f22fd29
Betina Kutsarova <betina.kutsarova@intellias.com>
2025-02-12 11:32:37 +0200

    add mock api and constructors to classes

--- RideSharingApp.js
+++ RideSharingApp.js
@@ -1,4 +1,4 @@
-const drivers = ['Boyan', 'Angel', 'Georgi'];
+const DRIVERS_API_URL = "https://jsonplaceholder.typicode.com/users";
 const users = [];
 
 class RideSharingApp {
@@ -24,21 +24,67 @@ class RideSharingApp {
 
 
 class User {
+    #balance;
+
+    constructor(name, balance) {
+        this.name = name;
+        this.#balance = balance;
+    }
+
+    getBalance() {
+        return this.#balance;
+    }
+
 
 }
 
 class PremiumUser extends User {
+    // has a discount
+    constructor(name, balance) {
+        super(name, balance)
+    }
 
 }
 
 class Driver {
+    #balance;
 
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.isAvailable = true;
+    }
 }
 
 class VIPDriver extends Driver {
-    
+    // more expensive ?
+    constructor(name, id, balance) {
+        super(name, id, balance);
+    }
 }
 
 class Ride {
+    constructor(user, pickupLocation, dropoffLocation) {
+        this.user = user;
+        this.pickupLocation = pickupLocation;
+        this.dropoffLocation = dropoffLocation;
+        this.status = "pending";
+        this.driver = null;
+        this.fare = this.calculateFare();
+    }
+
+    calculateFare() {
+        return 10;
+    }
+
+    assignDriver(driver) {
+        this.driver = driver;
+        this.status = "active";
+    }
+
+    complete() {
+        this.status = "completed";
+        console.log(`Ride completed. Fare: $${this.fare}`);
+    }
 
 }
